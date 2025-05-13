94434ca846432351cbaab87c9dfeb47e887a6f48
JaViKo500 <javikogutierrez64@gmail.com>
2025-05-12 23:13:47 -0500

    feat: add new category

--- src/GifExpertApp.tsx
+++ src/GifExpertApp.tsx
@@ -2,8 +2,15 @@ import { useState } from 'react';
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ]);
+  const onAddCategory = ( category: string ) => {
+    setCategories( [ category, ...categories ] );
+  }
   return (
-    <>  
+    <>
+      <h4>Gif Expert</h4>
+      <h5>Add a category</h5>
+      <button onClick={() => onAddCategory('Test')} >Add</button>
+      <h5>Categories</h5>
       <ol>
         {
           categories.map( ( category ) => {
