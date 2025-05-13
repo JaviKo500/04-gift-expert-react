2aa21b0f6b31c0d7dd70c6c5cdc8fe585343afe7
JaViKo500 <javikogutierrez64@gmail.com>
2025-05-12 22:51:19 -0500

    ui: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import './styles.css';
+
 import { GifExpertApp } from './GifExpertApp';
 
 createRoot(document.getElementById('root')!).render(
