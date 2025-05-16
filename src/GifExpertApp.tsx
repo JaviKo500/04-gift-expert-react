a04bd09a7ad205cc4b68cfb2ad841153a19ea7e5
JaViKo500 <javikogutierrez64@gmail.com>
2025-05-15 23:09:28 -0500

    fix: valid unique category

--- src/GifExpertApp.tsx
+++ src/GifExpertApp.tsx
@@ -5,6 +5,7 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState<string[]>([]);
 
    const onAddCategory = ( newCategory: string ) => {
+    if( categories.includes( newCategory ) ) return;
     setCategories( [ newCategory, ...categories ] );
   }
 
